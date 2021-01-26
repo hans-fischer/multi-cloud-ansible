5dfa05c1c2368a838b09f50c5b9231bc35f809a0
Hans Fischer <kontakt@hans-fischer.com>
2021-01-26 15:59:55 +0100

    Added Azure to test-group

--- start.sh
+++ start.sh
@@ -6,5 +6,9 @@ docker run -it \
     -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
     -e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
     -e GOOGLE_APPLICATION_CREDENTIALS="/root/packer/local_google_compute_service_account.json" \
+    -e AZURE_CLIENT_ID=$AZURE_CLIENT_ID \
+    -e AZURE_CLIENT_SECRET=$AZURE_CLIENT_SECRET \
+    -e AZURE_TENANT_ID=$AZURE_TENANT_ID \
+    -e AZURE_SUBSCRIPTION_ID=$AZURE_SUBSCRIPTION_ID \
     provision_worker \
     bash
